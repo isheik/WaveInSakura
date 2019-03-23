dc6282a7463efff625ddbbbc3110120b73d91c93
isheik <isheik808@gmail.com>
2019-03-22 23:54:36 -0700

    upload latest

--- WaveInSakura/kitty.cpp
+++ WaveInSakura/kitty.cpp
@@ -1,7 +1,8 @@
 #pragma comment(lib, "Winmm.lib")
 #include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "wav_media_player.h"
-
 #include "resource2.h"
 #define TITLE 	TEXT("Kitty on your lap")
 #define SRATE 	11025
@@ -10,34 +11,239 @@ WNDCLASSEX Wcl;
 const char Name[] = "Test app";
 HWND hwnd;
 
-	 WAVEFORMATEX wfe;
-	 HWAVEOUT hWaveOut;
-	 HWAVEIN hWaveIn;
-	 //LPSTR bWave;
-	 BYTE *bWave;
-	 WAVEHDR whdr;
+WAVEFORMATEX wfe;
+HWAVEOUT hWaveOut;
+HWAVEIN hWaveIn;
+//LPSTR bWave;
+BYTE *bWave1;
+BYTE *bWave2;
+BYTE *bSave;
+BYTE *bTmp;
+WAVEHDR whdr1;
+WAVEHDR whdr2;
+int dwLength = 0;
+int dwCount;
+BOOL blReset = FALSE;
 
-	MMRESULT rc;
+MMRESULT rc;
 
-// ### This callback does not work proparly cuz using WaveIn functions inside it, which is prohibited to cause deadlock
-//VOID CALLBACK waveInProc(HWAVEIN hwi, UINT uMsg, DWORD dwInstance,
-//	DWORD dwParam1, DWORD dwParam2)
-//{
-//	switch (uMsg)
-//	{
-//	case WIM_OPEN:
-//		OutputDebugStringA("open");
-//		break;
-//	case WIM_CLOSE:
-//		waveInUnprepareHeader(hWaveIn, &whdr, sizeof(WAVEHDR));
-		//break; // don't care
-	//case WIM_DATA:
+
+
+INT_PTR CALLBACK handleClientDialog(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDB_REC:
+			bWave1 = (BYTE*)malloc(SRATE);
+			bWave2 = (BYTE*)malloc(SRATE);
+
+			whdr1.lpData = (LPSTR)bWave1;
+			whdr1.dwBufferLength = SRATE;
+			whdr1.dwBytesRecorded = 0;
+			whdr1.dwFlags = 0;
+			whdr1.dwLoops = 1;
+			whdr1.lpNext = NULL;
+			whdr1.dwUser = 0;
+			whdr1.reserved = 0;
+
+			whdr2.lpData = (LPSTR)bWave2;
+			whdr2.dwBufferLength = SRATE;
+			whdr2.dwBytesRecorded = 0;
+			whdr2.dwFlags = 0;
+			whdr2.dwLoops = 1;
+			whdr2.lpNext = NULL;
+			whdr2.dwUser = 0;
+			whdr2.reserved = 0;
+
+			rc = waveInOpen(&hWaveIn, WAVE_MAPPER, &wfe,
+				(DWORD)hwnd, 0, CALLBACK_WINDOW);
+			//rc = waveInOpen(&hWaveIn, WAVE_MAPPER, &wfe,
+				//(DWORD)(VOID*)waveInProc, (DWORD)hwnd, CALLBACK_FUNCTION);
+			if (rc == MMSYSERR_NOERROR) {
+				OutputDebugStringA("good");
+				waveInPrepareHeader(hWaveIn, &whdr1, sizeof(WAVEHDR));
+				waveInPrepareHeader(hWaveIn, &whdr2, sizeof(WAVEHDR));
+
+			//	rc = waveInAddBuffer(hWaveIn, &whdr, sizeof(WAVEHDR));
+			//	if (rc == MMSYSERR_NOERROR)
+			//	{
+			//		OutputDebugStringA("Added buffer probably");
+			//	}
+			//	else 
+			//	{
+			//		switch (rc)
+			//		{
+			//		case MMSYSERR_INVALHANDLE:
+			//			OutputDebugStringA("invalid handle");
+			//			break;
+			//		case MMSYSERR_NODRIVER:
+			//			OutputDebugStringA("no driver");
+			//			break;
+			//		case MMSYSERR_NOMEM:
+			//			OutputDebugStringA("nomem");
+			//			break;
+			//		case WAVERR_UNPREPARED:
+			//			OutputDebugStringA("UNPREPARED");
+			//			break;
+			//		}
+			//	}
+
+			//	rc = waveInStart(hWaveIn);
+			//	if (rc == MMSYSERR_NOERROR)
+			//	{
+			//		OutputDebugStringA("Started");
+			//	}
+			//	else 
+			//	{
+			//		switch (rc)
+			//		{
+			//		case MMSYSERR_INVALHANDLE:
+			//			OutputDebugStringA("start invalid handle");
+			//			break;
+			//		case MMSYSERR_NODRIVER:
+			//			OutputDebugStringA("start no driver");
+			//			break;
+			//		case MMSYSERR_NOMEM:
+			//			OutputDebugStringA("start nomem");
+			//			break;
+			//		case WAVERR_UNPREPARED:
+			//			OutputDebugStringA("start UNPREPARED");
+			//			break;
+			//		}
+			//	}
+			//}
+			//else {
+			//	switch (rc) {
+			//	case MMSYSERR_ALLOCATED:
+			//		OutputDebugStringA("alloc");
+			//		break;
+			//	case MMSYSERR_BADDEVICEID:
+			//		OutputDebugStringA("bad");
+			//		break;
+			//	case MMSYSERR_NODRIVER:
+			//		OutputDebugStringA("NO DRIVER");
+			//		break;
+			//	case MMSYSERR_NOMEM:
+			//		OutputDebugStringA("NO mem");
+			//		break;
+			//	case WAVERR_BADFORMAT:
+			//		OutputDebugStringA("Bad form");
+			//		break;
+			//	case MMSYSERR_ERROR:
+			//		OutputDebugStringA("Unknown");
+			//		break;
+			//	default:
+			//		OutputDebugStringA("else");
+			//		break;
+			//	}
+			}
+
+			break;
+		case IDB_PLAY:
+			//waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfe,
+			//	(DWORD)hwnd, 0, CALLBACK_WINDOW);
+
+			//waveOutPrepareHeader(hWaveOut, &whdr, sizeof(WAVEHDR));
+			//waveOutWrite(hWaveOut, &whdr, sizeof(WAVEHDR));
+			whdr1.lpData = (LPSTR)bSave;
+			whdr1.dwBufferLength = dwLength;
+			whdr1.dwBytesRecorded = 0;
+			whdr1.dwFlags = WHDR_BEGINLOOP | WHDR_ENDLOOP;
+			whdr1.dwLoops = 1;
+			whdr1.lpNext = NULL;
+			whdr1.dwUser = 0;
+			whdr1.reserved = 0;
+
+			waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfe,
+				(DWORD)hwnd, 0, CALLBACK_WINDOW);
+			waveOutPrepareHeader(
+				hWaveOut, &whdr1, sizeof(WAVEHDR));
+			waveOutWrite(hWaveOut, &whdr1, sizeof(WAVEHDR));
+			break;
+		case IDB_REC_END:
+			blReset = TRUE;
+			waveInReset(hWaveIn);
+			break;
+		case IDB_PLAY_END:
+			waveOutReset(hWaveOut);
+			break;
+		default:
+			break;
+		}
+	case IDCANCEL:
+		EndDialog(hwndDlg, (INT_PTR)0);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	case WM_DESTROY:
+		//free(bWave);
+		if (bSave)
+		{
+			free(bSave);
+		}
+		PostQuitMessage(0);
+		return 0;
+	case WM_CREATE:
+		OutputDebugStringA("wm create");
+		//bWave = (LPSTR)malloc(SRATE * 3);
+
+		wfe.wFormatTag = WAVE_FORMAT_PCM;
+		wfe.nChannels = 1;
+		wfe.nSamplesPerSec = SRATE;
+		wfe.nAvgBytesPerSec = SRATE;
+		wfe.wBitsPerSample = 8;
+		//wfe.nBlockAlign = wfe.nChannels * wfe.wBitsPerSample / 8;
+		wfe.nBlockAlign = 1;
+		wfe.cbSize = 0;
+
+		return 0;
+	case WM_COMMAND:
+		switch (LOWORD(wp)) {
+		case DE_CLIENT:
+			DialogBox(Wcl.hInstance, MAKEINTRESOURCE(IDD_FORMVIEW), hwnd, handleClientDialog);
+			return 0;
+		}
+	case WIM_OPEN:
+		OutputDebugStringA("open");
+		dwLength = 0;
+		bSave = (BYTE*)realloc(bSave, 1);
+		waveInAddBuffer(hWaveIn, &whdr1, sizeof(WAVEHDR));
+		waveInAddBuffer(hWaveIn, &whdr2, sizeof(WAVEHDR));
+		waveInStart(hWaveIn);
+		break;
+	case WIM_DATA:
 		// post message to process this input block received
 		// NOTE: callback cannot call other waveform functions
 		//....................................................
 
 		//PostMessage((HWND)dwInstance, USR_INBLOCK, 0, dwParam1);
-		//OutputDebugStringA("data");
+		OutputDebugStringA("data");
+		bTmp = (BYTE*)realloc(bSave, dwLength + ((PWAVEHDR)lp)->dwBytesRecorded);
+		char sbuf[512];
+		sprintf_s(sbuf, "%d\n", ((PWAVEHDR)lp)->dwBytesRecorded);
+		OutputDebugString(sbuf);
+
+		if (blReset || !bTmp) {
+			waveInClose(hWaveIn);
+			blReset = FALSE;
+			return 0;
+		}
+
+		bSave = bTmp;
+		for (dwCount = 0; dwCount < ((PWAVEHDR)lp)->dwBytesRecorded; dwCount++)
+			*(bSave + dwLength + dwCount) = *(((PWAVEHDR)lp)->lpData + dwCount);
+		dwLength += ((PWAVEHDR)lp)->dwBytesRecorded;
+		waveInAddBuffer(hWaveIn, (PWAVEHDR)lp, sizeof(WAVEHDR));
+
 		//rc = waveInStop(hWaveIn);
 		//OutputDebugStringA("stop called");
 		//if (rc == MMSYSERR_NOERROR)
@@ -92,248 +298,14 @@ HWND hwnd;
 		//			break;
 		//	}
 		//}
-		//break;
-	//case WOM_OPEN:
-	//	OutputDebugStringA("wom open");
-	//	break;
-	//case WOM_DONE:
-	//	waveOutClose(hWaveOut);
-	//	break;
-	//case WOM_CLOSE:
-	//	waveOutUnprepareHeader(hWaveOut, &whdr, sizeof(WAVEHDR));
-	//	break;
-	//}
-//}
-
-VOID CALLBACK waveOutProc(HWAVEIN hwi, UINT uMsg, DWORD dwInstance,
-	DWORD dwParam1, DWORD dwParam2)
-{
-	switch (uMsg)
-	{
-	case WOM_OPEN:
-		OutputDebugStringA("wom open");
-		break;
-	case WOM_DONE:
-		OutputDebugStringA("done");
-		waveOutClose(hWaveOut);
-		break;
-	case WOM_CLOSE:
-		waveOutUnprepareHeader(hWaveOut, &whdr, sizeof(WAVEHDR));
-		break;
-	}
-}
-
-
-
-INT_PTR CALLBACK handleClientDialog(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return TRUE;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDB_REC:
-			rc = waveInOpen(&hWaveIn, WAVE_MAPPER, &wfe,
-				(DWORD)hwnd, 0, CALLBACK_WINDOW);
-			//rc = waveInOpen(&hWaveIn, WAVE_MAPPER, &wfe,
-				//(DWORD)(VOID*)waveInProc, (DWORD)hwnd, CALLBACK_FUNCTION);
-			if (rc == MMSYSERR_NOERROR) {
-				OutputDebugStringA("good");
-
-				waveInPrepareHeader(hWaveIn, &whdr, sizeof(WAVEHDR));
-				rc = waveInAddBuffer(hWaveIn, &whdr, sizeof(WAVEHDR));
-
-				if (rc == MMSYSERR_NOERROR)
-				{
-					OutputDebugStringA("Added buffer probably");
-				}
-				else 
-				{
-					switch (rc)
-					{
-					case MMSYSERR_INVALHANDLE:
-						OutputDebugStringA("invalid handle");
-						break;
-					case MMSYSERR_NODRIVER:
-						OutputDebugStringA("no driver");
-						break;
-					case MMSYSERR_NOMEM:
-						OutputDebugStringA("nomem");
-						break;
-					case WAVERR_UNPREPARED:
-						OutputDebugStringA("UNPREPARED");
-						break;
-					}
-				}
-
-				rc = waveInStart(hWaveIn);
-				if (rc == MMSYSERR_NOERROR)
-				{
-					OutputDebugStringA("Started");
-				}
-				else 
-				{
-					switch (rc)
-					{
-					case MMSYSERR_INVALHANDLE:
-						OutputDebugStringA("start invalid handle");
-						break;
-					case MMSYSERR_NODRIVER:
-						OutputDebugStringA("start no driver");
-						break;
-					case MMSYSERR_NOMEM:
-						OutputDebugStringA("start nomem");
-						break;
-					case WAVERR_UNPREPARED:
-						OutputDebugStringA("start UNPREPARED");
-						break;
-					}
-				}
-			}
-			else {
-				switch (rc) {
-				case MMSYSERR_ALLOCATED:
-					OutputDebugStringA("alloc");
-					break;
-				case MMSYSERR_BADDEVICEID:
-					OutputDebugStringA("bad");
-					break;
-				case MMSYSERR_NODRIVER:
-					OutputDebugStringA("NO DRIVER");
-					break;
-				case MMSYSERR_NOMEM:
-					OutputDebugStringA("NO mem");
-					break;
-				case WAVERR_BADFORMAT:
-					OutputDebugStringA("Bad form");
-					break;
-				case MMSYSERR_ERROR:
-					OutputDebugStringA("Unknown");
-					break;
-				default:
-					OutputDebugStringA("else");
-					break;
-				}
-			}
-
-			break;
-		case IDB_PLAY:
-			waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfe,
-				(DWORD)hwnd, 0, CALLBACK_WINDOW);
-			//rc = waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfe,
-				//(DWORD)(VOID*)waveOutProc, (DWORD)hwnd, CALLBACK_FUNCTION);
-
-			waveOutPrepareHeader(hWaveOut, &whdr, sizeof(WAVEHDR));
-			waveOutWrite(hWaveOut, &whdr, sizeof(WAVEHDR));
-			break;
-		default:
-			break;
-		}
-	case IDCANCEL:
-		EndDialog(hwndDlg, (INT_PTR)0);
-		return TRUE;
-	}
-	return FALSE;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
-	switch (msg) {
-	case WM_DESTROY:
-		free(bWave);
-		PostQuitMessage(0);
-		return 0;
-	case WM_CREATE:
-		OutputDebugStringA("wm create");
-		bWave = (BYTE*)malloc(SRATE * 5);
-		//bWave = (LPSTR)malloc(SRATE * 3);
-
-		wfe.wFormatTag = WAVE_FORMAT_PCM;
-		wfe.nChannels = 1;
-		wfe.nSamplesPerSec = SRATE;
-		wfe.nAvgBytesPerSec = SRATE;
-		wfe.wBitsPerSample = 8;
-		wfe.nBlockAlign = wfe.nChannels * wfe.wBitsPerSample / 8;
-
-		whdr.lpData = (LPSTR)bWave;
-		whdr.dwBufferLength = SRATE * 5;
-		whdr.dwLoops = 1;
-		return 0;
-	case WM_COMMAND:
-		switch (LOWORD(wp)) {
-		case DE_CLIENT:
-			DialogBox(Wcl.hInstance, MAKEINTRESOURCE(IDD_FORMVIEW), hwnd, handleClientDialog);
-			return 0;
-	}
-	case WIM_OPEN:
-		OutputDebugStringA("open");
 		break;
 	case WIM_CLOSE:
-		waveInUnprepareHeader(hWaveIn, &whdr, sizeof(WAVEHDR));
+		OutputDebugStringA("wim close");
+		waveInUnprepareHeader(hWaveIn, &whdr1, sizeof(WAVEHDR));
+		waveInUnprepareHeader(hWaveIn, &whdr2, sizeof(WAVEHDR));
+		free(bWave1);
+		free(bWave2);
 		break; // don't care
-	case WIM_DATA:
-		// post message to process this input block received
-		// NOTE: callback cannot call other waveform functions
-		//....................................................
-
-		//PostMessage((HWND)dwInstance, USR_INBLOCK, 0, dwParam1);
-		OutputDebugStringA("data");
-		rc = waveInStop(hWaveIn);
-		OutputDebugStringA("stop called");
-		if (rc == MMSYSERR_NOERROR)
-		{
-			OutputDebugStringA("stop success");
-		}
-		else
-		{
-			switch (rc)
-			{
-				case MMSYSERR_INVALHANDLE:
-					OutputDebugStringA("stop invalid handle");
-					break;
-				case MMSYSERR_NODRIVER:
-					OutputDebugStringA("stop no driver");
-					break;
-				case MMSYSERR_NOMEM:
-					OutputDebugStringA("stop nomem");
-					break;
-				case WAVERR_UNPREPARED:
-					OutputDebugStringA("stop UNPREPARED");
-					break;
-				default:
-					OutputDebugStringA("stop default");
-					break;
-			}
-		}
-		rc = waveInClose(hWaveIn);
-		OutputDebugStringA("close called");
-		if (rc == MMSYSERR_NOERROR)
-		{
-			OutputDebugStringA("close success");
-		}
-		else
-		{
-			switch (rc)
-			{
-				case MMSYSERR_INVALHANDLE:
-					OutputDebugStringA("close invalid handle");
-					break;
-				case MMSYSERR_NODRIVER:
-					OutputDebugStringA("close no driver");
-					break;
-				case MMSYSERR_NOMEM:
-					OutputDebugStringA("close nomem");
-					break;
-				case WAVERR_UNPREPARED:
-					OutputDebugStringA("close UNPREPARED");
-					break;
-				default:
-					OutputDebugStringA("close default");
-					break;
-			}
-		}
-		break;
 	case WOM_OPEN:
 		OutputDebugStringA("wom open");
 		break;
@@ -342,7 +314,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 		waveOutClose(hWaveOut);
 		break;
 	case WOM_CLOSE:
-		waveOutUnprepareHeader(hWaveOut, &whdr, sizeof(WAVEHDR));
+		waveOutUnprepareHeader(hWaveOut, &whdr1, sizeof(WAVEHDR));
 		break;
 	}
 
--- WaveInSakura/resource2.h
+++ WaveInSakura/resource2.h
@@ -3,8 +3,10 @@
 // Used by audiodlg.rc
 //
 #define IDD_FORMVIEW                    101
-#define IDB_REC                    1001
-#define IDB_PLAY                     1002
+#define IDB_REC                         1001
+#define IDB_PLAY                        1002
+#define IDB_REC_END                     1003
+#define IDB_PLAY_END                     1004
 
 // Next default values for new objects
 // 
@@ -12,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
